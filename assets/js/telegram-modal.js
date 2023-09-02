f2cb2d7346a3b27f25101b8254e51e49814d6755
oleh <oleh.a.falko@gmail.com>
2023-09-02 09:47:16 +0200

    fix: modal

--- assets/js/telegram-modal.js
+++ assets/js/telegram-modal.js
@@ -1,5 +1,5 @@
 const tgModal = document.querySelector('.telegram-modal');
-const closeBtn = document.querySelector('.close-btn');
+const closeBtn = document.querySelector('.close-form');
 const whiteBg = document.querySelector('.fixed-white-bg');
 
 let tgModalClosed = localStorage.getItem('tgModalClosed')  === 'true';
@@ -17,7 +17,8 @@ window.addEventListener("scroll", () => {
 });
 
 closeBtn.addEventListener('click', () => {
-    tgModal.classList.remove("show-modal");
+    tgModal.style.display = 'none';
+    whiteBg.classList.remove("active");
     tgModalClosed = true;
     localStorage.setItem('tgModalClosed', 'true');
 });
