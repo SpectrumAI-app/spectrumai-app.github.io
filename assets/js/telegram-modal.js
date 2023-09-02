ee852fb9373fcaaf70acdc8fb136d627042dc184
oleh <oleh.a.falko@gmail.com>
2023-09-02 09:38:17 +0200

    fix: modal

--- assets/js/telegram-modal.js
+++ assets/js/telegram-modal.js
@@ -1,14 +1,18 @@
 const tgModal = document.querySelector('.telegram-modal');
 const closeBtn = document.querySelector('.close-btn');
+const whiteBg = document.querySelector('.fixed-white-bg');
 
 let tgModalClosed = localStorage.getItem('tgModalClosed')  === 'true';
+
 window.addEventListener("scroll", () => {
     // Check if the user has scrolled down a certain amount (e.g., 100 pixels)
     if (window.scrollY > 500 && !tgModalClosed) {
         // Add a class to hide the div
-       tgModal.style.display = 'none';
+       tgModal.style.display = 'block';
+       whiteBg.classList.add("active");
     } else {
-        tgModal.style.display = 'block';
+        tgModal.style.display = 'none';
+        whiteBg.classList.remove("active");
     }
 });
 
